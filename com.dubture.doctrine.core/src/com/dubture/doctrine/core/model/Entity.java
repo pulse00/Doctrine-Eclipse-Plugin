03964923a66af5d5323aeafbb3719e5f3cd36408
Robert Gruendler <robert@dubture.com>
2011-10-21 11:59:17 +0200

    added entity source type

---
+++ com.dubture.doctrine.core/src/com/dubture/doctrine/core/model/Entity.java
@@ -0,0 +1,66 @@
+package com.dubture.doctrine.core.model;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.dltk.ast.Modifiers;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.internal.core.ModelElement;
+import org.eclipse.dltk.internal.core.SourceType;
+import org.eclipse.dltk.internal.core.SourceTypeElementInfo;
+import org.eclipse.dltk.internal.core.hierarchy.FakeType;
+
+@SuppressWarnings("restriction")
+public class Entity extends SourceType {
+
+	public Entity(ModelElement parent, String name) {
+		super(parent, name);
+
+	}
+
+	@Override
+	public int getFlags() throws ModelException {
+
+		return Modifiers.AccPublic;
+	}
+
+	@Override
+	public Object getElementInfo() throws ModelException {
+
+		System.err.println("element info");
+		return new FakeTypeElementInfo();
+	}
+
+	@Override
+	protected Object openWhenClosed(Object info, IProgressMonitor monitor)
+			throws ModelException {
+
+		return new FakeTypeElementInfo();
+
+	}
+
+	@Override
+	public ISourceModule getSourceModule() {
+
+		return super.getSourceModule();
+	}
+
+	@Override
+	public IModelElement getParent() {
+
+		// avoid showing the same name twice in each codeassist
+		// popup row, ie:
+		// AcmeDemoBundle - AcmeDemoBundle
+		return new FakeType(parent, "");		
+
+	}
+	
+	public class FakeTypeElementInfo extends SourceTypeElementInfo {
+		
+		@Override
+		public String getFileName() {
+
+			return "";
+		}		
+	}
+}
