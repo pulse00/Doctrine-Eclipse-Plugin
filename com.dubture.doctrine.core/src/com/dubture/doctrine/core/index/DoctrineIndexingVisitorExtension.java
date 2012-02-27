34921b91b1bc3cb96d3b69196f3452eff01dffd4
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2012-02-27 09:04:34 -0500

    removed old AnnotationNodeVisitor API  * These API has been removed in favor of the new way to work with Annotation,    the Annotation model. Every old methods are now part of the Annotation    class which can be requested from a visitor by doing visitor.getAnnotation()

--- com.dubture.doctrine.core/src/com/dubture/doctrine/core/index/DoctrineIndexingVisitorExtension.java
+++ com.dubture.doctrine.core/src/com/dubture/doctrine/core/index/DoctrineIndexingVisitorExtension.java
@@ -17,6 +17,8 @@ import org.eclipse.php.internal.core.compiler.ast.nodes.PHPDocBlock;
 
 import com.dubture.doctrine.core.log.Logger;
 import com.dubture.doctrine.core.model.IDoctrineModelElement;
+import com.dubture.symfony.annotation.model.ArgumentValueType;
+import com.dubture.symfony.annotation.model.IArgumentValue;
 import com.dubture.symfony.annotation.parser.antlr.AnnotationLexer;
 import com.dubture.symfony.annotation.parser.antlr.AnnotationParser;
 import com.dubture.symfony.annotation.parser.antlr.error.IAnnotationErrorReporter;
@@ -29,126 +31,127 @@ import com.dubture.symfony.annotation.parser.tree.visitor.AnnotationNodeVisitor;
  * Visits Doctrine Annotations.
  *
  * Currently indexes Entity classes and their corresponding
- * repositoryClass. 
- * 
+ * repositoryClass.
+ *
  * @author Robert Gruendler <r.gruendler@gmail.com>
  *
  */
 @SuppressWarnings("restriction")
 public class DoctrineIndexingVisitorExtension extends
-		PhpIndexingVisitorExtension {
+        PhpIndexingVisitorExtension {
 
-	private NamespaceDeclaration namespace;
-	
-	@Override
-	public void setSourceModule(ISourceModule module) {
+    private NamespaceDeclaration namespace;
 
-		super.setSourceModule(module);
+    @Override
+    public void setSourceModule(ISourceModule module) {
 
-	}
-	
-	@Override
-	public boolean visit(TypeDeclaration s) throws Exception {
+        super.setSourceModule(module);
 
-		if (s instanceof ClassDeclaration) {
-			
-			ClassDeclaration clazz = (ClassDeclaration) s;		
-			parseClassDocBlock(clazz);
-			
-		} else if (s instanceof NamespaceDeclaration) {
-			
-			NamespaceDeclaration ns = (NamespaceDeclaration) s;
-			namespace = ns;
-		}
-		
-		return true;
-	}
-	
-	
-	protected void parseClassDocBlock(ClassDeclaration clazz) {
+    }
 
-		PHPDocBlock phpDoc = clazz.getPHPDoc();
-		
-		if (phpDoc == null)
-			return;
-		
-		String comment = "";
-		
-		comment = phpDoc.getLongDescription();
-		
-		if (comment == null || comment.length() == 0)
-			comment = phpDoc.getShortDescription();
-		
-		BufferedReader buffer = new BufferedReader(new StringReader(comment));
+    @Override
+    public boolean visit(TypeDeclaration s) throws Exception {
 
-		try {
+        if (s instanceof ClassDeclaration) {
 
-			String line;
+            ClassDeclaration clazz = (ClassDeclaration) s;
+            parseClassDocBlock(clazz);
 
-			while((line = buffer.readLine()) != null) {
-		
-				int start = line.indexOf('@');
-				int end = line.length()-1;
-				
-				if (start == -1) {
-					continue;
-				}
-				String annotation = line.substring(start, end+1);
-				CharStream content = new ANTLRStringStream(annotation);
-				
-				IAnnotationErrorReporter reporter = new IAnnotationErrorReporter() {
-					
-					@Override
-					public void reportError(String header, String message,
-							RecognitionException e) {
-						
-					}
-				};
-				
-				AnnotationLexer lexer = new AnnotationLexer(content, reporter);
-				
-				AnnotationParser parser = new AnnotationParser(new CommonTokenStream(lexer));
-				parser.setErrorReporter(reporter);
-				
-				parser.setTreeAdaptor(new AnnotationCommonTreeAdaptor());
-				AnnotationParser.annotation_return root;
-				
-				root = parser.annotation();
-				AnnotationCommonTree tree = (AnnotationCommonTree) root.getTree();
-				AnnotationNodeVisitor visitor = new AnnotationNodeVisitor();
-				
-				if (tree == null) {					
-					return;					
-				}
-				
-				tree.accept(visitor);
-				
-				if ("Entity".equals(visitor.getClassName())) {
+        } else if (s instanceof NamespaceDeclaration) {
 
-					String qualifier = null;
-					if (namespace != null) {						
-						qualifier = namespace.getName();
-					}
-					
-					ReferenceInfo entityInfo = new ReferenceInfo(IDoctrineModelElement.ENTITY, clazz.sourceStart(), clazz.sourceEnd(), clazz.getName(), null, qualifier);
-					
-					Logger.debugMSG("indexing entity: " + clazz.getName() + " => " + qualifier); 
-					requestor.addReference(entityInfo);
-					
-					String repo = visitor.getArgument("repositoryClass");
-					
-					if (repo == null)
-						return;
+            NamespaceDeclaration ns = (NamespaceDeclaration) s;
+            namespace = ns;
+        }
 
-					ReferenceInfo info = new ReferenceInfo(IDoctrineModelElement.REPOSITORY_CLASS, clazz.sourceStart(), clazz.sourceEnd(), clazz.getName(), repo, qualifier);
-					
-					Logger.debugMSG("indexing repository class: " + clazz.getName() + " => " + repo); 
-					requestor.addReference(info);
+        return true;
+    }
 
-				}
-			}			
-		} catch (Exception e) {
-			Logger.logException(e);			
-		}
-	}
+
+    protected void parseClassDocBlock(ClassDeclaration clazz) {
+
+        PHPDocBlock phpDoc = clazz.getPHPDoc();
+
+        if (phpDoc == null)
+            return;
+
+        String comment = "";
+
+        comment = phpDoc.getLongDescription();
+
+        if (comment == null || comment.length() == 0)
+            comment = phpDoc.getShortDescription();
+
+        BufferedReader buffer = new BufferedReader(new StringReader(comment));
+
+        try {
+
+            String line;
+
+            while((line = buffer.readLine()) != null) {
+
+                int start = line.indexOf('@');
+                int end = line.length()-1;
+
+                if (start == -1) {
+                    continue;
+                }
+                String annotation = line.substring(start, end+1);
+                CharStream content = new ANTLRStringStream(annotation);
+
+                IAnnotationErrorReporter reporter = new IAnnotationErrorReporter() {
+
+                    @Override
+                    public void reportError(String header, String message,
+                            RecognitionException e) {
+
+                    }
+                };
+
+                AnnotationLexer lexer = new AnnotationLexer(content, reporter);
+
+                AnnotationParser parser = new AnnotationParser(new CommonTokenStream(lexer));
+                parser.setErrorReporter(reporter);
+
+                parser.setTreeAdaptor(new AnnotationCommonTreeAdaptor());
+                AnnotationParser.annotation_return root;
+
+                root = parser.annotation();
+                AnnotationCommonTree tree = (AnnotationCommonTree) root.getTree();
+                AnnotationNodeVisitor visitor = new AnnotationNodeVisitor();
+
+                if (tree == null) {
+                    return;
+                }
+
+                tree.accept(visitor);
+
+                if ("Entity".equals(visitor.getAnnotation().getClassName())) {
+
+                    String qualifier = null;
+                    if (namespace != null) {
+                        qualifier = namespace.getName();
+                    }
+
+                    ReferenceInfo entityInfo = new ReferenceInfo(IDoctrineModelElement.ENTITY, clazz.sourceStart(), clazz.sourceEnd(), clazz.getName(), null, qualifier);
+
+                    Logger.debugMSG("indexing entity: " + clazz.getName() + " => " + qualifier);
+                    requestor.addReference(entityInfo);
+
+                    IArgumentValue repoArgumentValue = visitor.getAnnotation().getArgumentValue("repositoryClass");
+
+                    if (repoArgumentValue == null || repoArgumentValue.getType() != ArgumentValueType.STRING)
+                        return;
+
+                    String repo = (String) repoArgumentValue.getValue();
+                    ReferenceInfo info = new ReferenceInfo(IDoctrineModelElement.REPOSITORY_CLASS, clazz.sourceStart(), clazz.sourceEnd(), clazz.getName(), repo, qualifier);
+
+                    Logger.debugMSG("indexing repository class: " + clazz.getName() + " => " + repo);
+                    requestor.addReference(info);
+
+                }
+            }
+        } catch (Exception e) {
+            Logger.logException(e);
+        }
+    }
 }
