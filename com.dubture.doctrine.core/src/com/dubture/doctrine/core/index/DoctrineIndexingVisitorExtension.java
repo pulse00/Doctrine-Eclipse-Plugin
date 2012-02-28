5eb3e0dc26ea3881483dbb45035f18c8f3836c5e
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2012-02-28 08:34:31 -0500

    now using the new annotation comment parser  * There is some duplication from the symfony core project

--- com.dubture.doctrine.core/src/com/dubture/doctrine/core/index/DoctrineIndexingVisitorExtension.java
+++ com.dubture.doctrine.core/src/com/dubture/doctrine/core/index/DoctrineIndexingVisitorExtension.java
@@ -1,30 +1,24 @@
 package com.dubture.doctrine.core.index;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
 
-import org.antlr.runtime.ANTLRStringStream;
-import org.antlr.runtime.CharStream;
-import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.RecognitionException;
 import org.eclipse.dltk.ast.declarations.TypeDeclaration;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.index2.IIndexingRequestor.ReferenceInfo;
 import org.eclipse.php.core.index.PhpIndexingVisitorExtension;
+import org.eclipse.php.internal.core.ast.nodes.Comment;
+import org.eclipse.php.internal.core.codeassist.strategies.PHPDocTagStrategy;
 import org.eclipse.php.internal.core.compiler.ast.nodes.ClassDeclaration;
 import org.eclipse.php.internal.core.compiler.ast.nodes.NamespaceDeclaration;
 import org.eclipse.php.internal.core.compiler.ast.nodes.PHPDocBlock;
 
 import com.dubture.doctrine.core.log.Logger;
 import com.dubture.doctrine.core.model.IDoctrineModelElement;
+import com.dubture.symfony.annotation.model.Annotation;
 import com.dubture.symfony.annotation.model.ArgumentValueType;
 import com.dubture.symfony.annotation.model.IArgumentValue;
-import com.dubture.symfony.annotation.parser.antlr.AnnotationLexer;
-import com.dubture.symfony.annotation.parser.antlr.AnnotationParser;
-import com.dubture.symfony.annotation.parser.antlr.error.IAnnotationErrorReporter;
-import com.dubture.symfony.annotation.parser.tree.AnnotationCommonTree;
-import com.dubture.symfony.annotation.parser.tree.AnnotationCommonTreeAdaptor;
-import com.dubture.symfony.annotation.parser.tree.visitor.AnnotationNodeVisitor;
+import com.dubture.symfony.annotation.parser.AnnotationCommentParser;
 
 
 /**
@@ -44,114 +38,89 @@ public class DoctrineIndexingVisitorExtension extends
 
     @Override
     public void setSourceModule(ISourceModule module) {
-
         super.setSourceModule(module);
-
     }
 
     @Override
-    public boolean visit(TypeDeclaration s) throws Exception {
-
-        if (s instanceof ClassDeclaration) {
-
-            ClassDeclaration clazz = (ClassDeclaration) s;
-            parseClassDocBlock(clazz);
-
-        } else if (s instanceof NamespaceDeclaration) {
-
-            NamespaceDeclaration ns = (NamespaceDeclaration) s;
-            namespace = ns;
+    public boolean visit(TypeDeclaration typeDeclaration) throws Exception {
+        if (typeDeclaration instanceof ClassDeclaration) {
+            ClassDeclaration classDeclaration = (ClassDeclaration) typeDeclaration;
+            processClassDeclaration(classDeclaration);
+        } else if (typeDeclaration instanceof NamespaceDeclaration) {
+            NamespaceDeclaration namespaceDeclaration = (NamespaceDeclaration) typeDeclaration;
+            namespace = namespaceDeclaration;
         }
 
         return true;
     }
 
+    protected void processClassDeclaration(ClassDeclaration classDeclaration) {
+        String[] includedClassNames = new String[]{"Entity"};
+        List<Annotation> annotations = extractAnnotations(classDeclaration, includedClassNames);
 
-    protected void parseClassDocBlock(ClassDeclaration clazz) {
-
-        PHPDocBlock phpDoc = clazz.getPHPDoc();
-
-        if (phpDoc == null)
+        if (annotations.size() < 1) {
             return;
+        }
 
-        String comment = "";
+        // Take only the last annotation
+        Annotation annotation = annotations.get(annotations.size() - 1);
 
-        comment = phpDoc.getLongDescription();
+        String qualifier = null;
+        if (namespace != null) {
+            qualifier = namespace.getName();
+        }
 
-        if (comment == null || comment.length() == 0)
-            comment = phpDoc.getShortDescription();
+        ReferenceInfo entityInfo = new ReferenceInfo(IDoctrineModelElement.ENTITY,
+                                                     classDeclaration.sourceStart(),
+                                                     classDeclaration.sourceEnd(),
+                                                     classDeclaration.getName(),
+                                                     null,
+                                                     qualifier);
 
-        BufferedReader buffer = new BufferedReader(new StringReader(comment));
+        Logger.debugMSG("indexing entity: " + classDeclaration.getName() + " => " + qualifier);
+        requestor.addReference(entityInfo);
 
+        IArgumentValue repoArgumentValue = annotation.getArgumentValue("repositoryClass");
+        if (repoArgumentValue == null || repoArgumentValue.getType() != ArgumentValueType.STRING) {
+            return;
+        }
+
+        String repositoryClass = (String) repoArgumentValue.getValue();
+        ReferenceInfo repositoryInfo = new ReferenceInfo(IDoctrineModelElement.REPOSITORY_CLASS,
+                                                         classDeclaration.sourceStart(),
+                                                         classDeclaration.sourceEnd(),
+                                                         classDeclaration.getName(),
+                                                         repositoryClass,
+                                                         qualifier);
+
+        Logger.debugMSG("indexing repository class: " + classDeclaration.getName() + " => " + repositoryClass);
+        requestor.addReference(repositoryInfo);
+    }
+
+    // FIXME: This has been copied from AnnotationUtils that can be found in com.dubture.symfony.core
+    protected List<Annotation> extractAnnotations(ClassDeclaration classDeclaration, String[] includedClassNames) {
         try {
-
-            String line;
-
-            while((line = buffer.readLine()) != null) {
-
-                int start = line.indexOf('@');
-                int end = line.length()-1;
-
-                if (start == -1) {
-                    continue;
-                }
-                String annotation = line.substring(start, end+1);
-                CharStream content = new ANTLRStringStream(annotation);
-
-                IAnnotationErrorReporter reporter = new IAnnotationErrorReporter() {
-
-                    @Override
-                    public void reportError(String header, String message,
-                            RecognitionException e) {
-
-                    }
-                };
-
-                AnnotationLexer lexer = new AnnotationLexer(content, reporter);
-
-                AnnotationParser parser = new AnnotationParser(new CommonTokenStream(lexer));
-                parser.setErrorReporter(reporter);
-
-                parser.setTreeAdaptor(new AnnotationCommonTreeAdaptor());
-                AnnotationParser.annotation_return root;
-
-                root = parser.annotation();
-                AnnotationCommonTree tree = (AnnotationCommonTree) root.getTree();
-                AnnotationNodeVisitor visitor = new AnnotationNodeVisitor();
-
-                if (tree == null) {
-                    return;
-                }
-
-                tree.accept(visitor);
-
-                if ("Entity".equals(visitor.getAnnotation().getClassName())) {
-
-                    String qualifier = null;
-                    if (namespace != null) {
-                        qualifier = namespace.getName();
-                    }
-
-                    ReferenceInfo entityInfo = new ReferenceInfo(IDoctrineModelElement.ENTITY, clazz.sourceStart(), clazz.sourceEnd(), clazz.getName(), null, qualifier);
-
-                    Logger.debugMSG("indexing entity: " + clazz.getName() + " => " + qualifier);
-                    requestor.addReference(entityInfo);
-
-                    IArgumentValue repoArgumentValue = visitor.getAnnotation().getArgumentValue("repositoryClass");
-
-                    if (repoArgumentValue == null || repoArgumentValue.getType() != ArgumentValueType.STRING)
-                        return;
-
-                    String repo = (String) repoArgumentValue.getValue();
-                    ReferenceInfo info = new ReferenceInfo(IDoctrineModelElement.REPOSITORY_CLASS, clazz.sourceStart(), clazz.sourceEnd(), clazz.getName(), repo, qualifier);
-
-                    Logger.debugMSG("indexing repository class: " + clazz.getName() + " => " + repo);
-                    requestor.addReference(info);
-
-                }
+            PHPDocBlock comment = classDeclaration.getPHPDoc();
+            if (comment == null || comment.getCommentType() != Comment.TYPE_PHPDOC) {
+                return new LinkedList<Annotation>();
             }
-        } catch (Exception e) {
-            Logger.logException(e);
+
+            int commentStartOffset = comment.sourceStart();
+            String commentSource = comment.getLongDescription();
+            if (commentSource.length() == 0) {
+                commentSource = comment.getShortDescription();
+            }
+
+            AnnotationCommentParser parser = new AnnotationCommentParser(commentSource, commentStartOffset);
+            parser.setExcludedClassNames(PHPDocTagStrategy.PHPDOC_TAGS);
+            if (includedClassNames != null) {
+                parser.setIncludedClassNames(includedClassNames);
+            }
+
+            return parser.parse();
+        } catch (Exception exception) {
+            Logger.logException("Unable to extract annotations from  class " + classDeclaration.getName(), exception);
+            return new LinkedList<Annotation>();
         }
     }
 }
