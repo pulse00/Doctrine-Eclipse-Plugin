d5726a8936d29e5bc915e38674883027a002904c
Robert Gruendler <robert@dubture.com>
2012-03-14 20:51:14 +0100

    cleaned up xml parser a bit

--- com.dubture.doctrine.core/src/com/dubture/doctrine/core/index/XmlMappingParser.java
+++ com.dubture.doctrine.core/src/com/dubture/doctrine/core/index/XmlMappingParser.java
@@ -7,14 +7,12 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import com.dubture.doctrine.core.log.Logger;
 import com.dubture.doctrine.core.model.Entity;
 
 /**
@@ -22,54 +20,45 @@ import com.dubture.doctrine.core.model.Entity;
  */
 public class XmlMappingParser
 {
-
     private XPath xPath;
     private Document doc;
     
-    public XmlMappingParser(InputStream file) throws Exception {
-
+    public XmlMappingParser(InputStream file) throws Exception
+    {
         xPath = XPathFactory.newInstance().newXPath();
         doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
-
     }
 
-    public void parse()
+    public void parse() throws Exception
     {
-        try {
+        String servicePath = "/doctrine-mapping/entity";
+        NodeList routeNodes = getNodes(servicePath);
 
-            String servicePath = "/doctrine-mapping/entity";
-            NodeList routeNodes = getNodes(servicePath);
+        for (int i = 0; i < routeNodes.getLength(); i++) {
 
-            for (int i = 0; i < routeNodes.getLength(); i++) {
+            Node node = routeNodes.item(i);             
+            NamedNodeMap atts = node.getAttributes();
 
-                Node node = routeNodes.item(i);             
-                NamedNodeMap atts = node.getAttributes();
+            //TODO: index repository class
+            String repo = null;
+            String phpClass = null;
 
-                //TODO: index repository class
-                String repo = null;
-                String phpClass = null;
+            for (int j = 0; j < atts.getLength(); j++) {
 
-                for (int j = 0; j < atts.getLength(); j++) {
+                Attr attr = (Attr) atts.item(j);
+                String key = attr.getName();
+                if (key.equals("repository-class"))
+                    repo = attr.getValue();
+                else if (key.equals("name"))
+                    phpClass = attr.getValue();
 
-                    Attr attr = (Attr) atts.item(j);
-
-                    String key = attr.getName();
-                    if (key.equals("repository-class"))
-                        repo = attr.getValue();
-                    else if (key.equals("name"))
-                        phpClass = attr.getValue();
-
-                }
-
-                if (phpClass != null) {
-                    Entity entity = new Entity(null, phpClass);
-                    DoctrineBuilder.addPendingEntity(entity);
-                }
             }
 
-        } catch (Exception e) {
-            Logger.logException(e);
-        }        
+            if (phpClass != null) {
+                Entity entity = new Entity(null, phpClass);
+                DoctrineBuilder.addPendingEntity(entity);
+            }
+        }
     }
 
     private NodeList getNodes(String path) throws Exception {
@@ -79,5 +68,4 @@ public class XmlMappingParser
         return (NodeList) result;
 
     }
-
 }
